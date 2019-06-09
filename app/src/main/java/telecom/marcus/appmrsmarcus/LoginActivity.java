bae2b0613fa6536f278b2cd8da24335bcdaec8fe
MarcusTelecom <marcus.ufsj@gmail.com>
2019-06-08 21:12:39 -0300

    Função Login

--- app/src/main/java/telecom/marcus/appmrsmarcus/LoginActivity.java
+++ app/src/main/java/telecom/marcus/appmrsmarcus/LoginActivity.java
@@ -1,13 +1,39 @@
 package telecom.marcus.appmrsmarcus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ProgressBar loading;
+    private AppCompatEditText registration, password;
+    private AppCompatButton btn_login;
+    private static String URL_LOGIN = "http://192.168.2.120/bd_mrs/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +41,94 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle(R.string.txt_bt_login);
+
+        //sessionManager = new SessionManager(this);
+
+        loading = findViewById(R.id.loading);
+        registration = findViewById(R.id.registration);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mRegistration = registration.getText().toString().trim();
+                String mPassword = password.getText().toString().trim();
+
+                login(mRegistration,mPassword);
+            }
+        });
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                intent.putExtra("newUser", true);
+                startActivity(intent);
             }
         });
+    }
+
+    private void login(final String registration, final String password) {
+        loading.setVisibility(View.VISIBLE);
+        btn_login.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String sucess = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                            if (sucess.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+                                    //String name = object.getString("name").trim();
+                                    //String email = object.getString("email").trim();
+                                    String id = object.getString("id").trim(); Toast.makeText(getApplicationContext(),id,Toast.LENGTH_LONG).show();
+
+                                    //sessionManager.createSession(id);
+
+                                    //Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+
+                                    //startActivity(intent);
+                                    loading.setVisibility(View.GONE);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loading.setVisibility(View.GONE);
+                            btn_login.setVisibility(View.VISIBLE);
+                            Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos " + e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.setVisibility(View.GONE);
+                        btn_login.setVisibility(View.VISIBLE);
+                        Toast.makeText(getApplicationContext(), "Error " + error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("registration", registration);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 }
